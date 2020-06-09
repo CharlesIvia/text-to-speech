b94f7a37d6c36e7af157314124d1f75a1d117de6
Charles Ivia <iviacharles@gmail.com>
2020-06-09 21:30:26 +0300

    Fetched dom elements and set variables

---
+++ script.js
@@ -0,0 +1,7 @@
+//Fetch dom elements and set variables
+const main = document.querySelector("main");
+const voicesSelect = document.getElementById("voices");
+const textarea = document.getElementById("text");
+const readBtn = document.getElementById("read");
+const toggleBtn = document.getElementById("toggle");
+const closeBtn = document.getElementById("close");
